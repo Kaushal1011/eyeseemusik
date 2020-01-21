bd874f7db44441f0f348bf7db52c5fa25363985e
Kaushal1011 <kaushalpatil10@gmail.com>
2020-01-22 01:53:16 +0530

    Audio OSmething

--- src/function/animation.ts
+++ src/function/animation.ts
@@ -1,27 +1,40 @@
 import * as THREE from 'three';
 
+
 export default function animation(){
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-500,500,500);
+    scene.add(light);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     let canvas:HTMLCanvasElement = renderer.domElement;
     canvas.id = 'clouds'
     document.body.appendChild( renderer.domElement );
+    
+    var audio = document.getElementById("player") as HTMLAudioElement;
+    
+    var audioCtx=new(window.AudioContext)();
+    audioCtx.createMediaElementSource(audio);
+    var analyser=audioCtx.createAnalyser();
+    
+    analyser.fftSize=1024;
+    var bufferLength=analyser.frequencyBinCount;
+    var dataArray=new Uint8Array(bufferLength);
+    analyser.getByteFrequencyData(dataArray);
+    
 
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    camera.position.z = 5;
-
+    
+    camera.position.z = 250;
+    
     var animate = function () {
         requestAnimationFrame( animate );
-
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        analyser.getByteFrequencyData(dataArray);
+        // console.log(dataArray)
+        
 
         renderer.render( scene, camera );
     };
