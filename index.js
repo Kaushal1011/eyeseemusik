09339b265e175998cdb7487030faeb5cbadaa339
Kaushal1011 <kaushalpatil10@gmail.com>
2021-01-27 19:57:00 +0530

    water in threejs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(__dirname + "/src/"));
+app.use("/eyeseemusik/", express.static(__dirname + "/src/landing.html"));
 app.use(
     "/build/",
     express.static(path.join(__dirname, "node_modules/three/build"))
@@ -11,5 +12,7 @@ app.use(
     "/jsm/",
     express.static(path.join(__dirname, "node_modules/three/examples/jsm"))
 );
+
+app.use("/textures/", express.static(path.join(__dirname, "/src/textures")));
 
 app.listen(3000, () => console.log("Visit http://127.0.0.1:3000"));
